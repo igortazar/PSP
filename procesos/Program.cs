e6a6960de5c0f46a32ae2e0bc6e2aef03feb95e6
unai tercero <utercero@gmail.com>
2019-08-01 17:43:37 +0200

    primer proyecto del curso

---
+++ procesos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace procesos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
