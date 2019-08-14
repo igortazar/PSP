43c82b6432a33737f74c25406610e278248590b3
unai tercero <utercero@gmail.com>
2019-08-14 12:27:15 +0200

    comunicacion primeros pasos

---
+++ Comunicacion/ComunicacionPrimerosPaso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+namespace ComunicacionPrimerosPaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("www.google.es");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            Console.WriteLine("La direcion de google es: {0}",ipAddress.ToString());
+            ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            ipAddress = ipHostInfo.AddressList[0];
+            Console.WriteLine("La direcion de esta maquina es: {0}", ipAddress);
+
+        }
+    }
+}
